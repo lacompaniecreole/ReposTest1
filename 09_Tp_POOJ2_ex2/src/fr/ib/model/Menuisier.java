7fa12e8aceb6fa1a2d176430f2a60dc793b2892e
altrangroupe8 <altrangroupe8@gmail.com>
2019-06-27 14:19:22 +0200

    salut  c steven

--- 09_Tp_POOJ2_ex2/src/fr/ib/model/Menuisier.java
+++ 09_Tp_POOJ2_ex2/src/fr/ib/model/Menuisier.java
@@ -15,7 +15,7 @@ public class Menuisier extends Personne {
 //toString
 	@Override
 	public String toString() {
-		return "Menuisier [id=" + id + ", nom=" + nom + "]";
+		return "Menuisier [id=" + id + ", nom888888888=" + nom + "]";
 	}	
 	
 }
